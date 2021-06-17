268da759d6521ea87e20e4cb8982a83f1eec0168
Laaurent <laurent-quenel@hotmail.Fr>
2021-06-17 09:31:54 +0200

    setup vuejs

---
+++ resources/js/components/MonComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <!-- Le template -->
+    <section class="container">
+      <h1>Mon composant MonComponent.vue</h1>
+      <p>
+        Clicks : <strong>{{ clicks }}</strong>
+      </p>
+      <button @click="ajouteClick">clique sur moi</button>
+    </section>
+  </div>
+</template>
+
+<script>
+// Le script
+export default {
+  name: "mon-component",
+  data() {
+    return {
+      clicks: 0,
+    };
+  },
+  methods: {
+    // Incrémentation de "clicks"
+    ajouteClick() {
+      this.clicks++;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
